a1982b9ed589cad7ab6a824e5de95e82816d32bc
Lucas Godoy <2lucasg@gmail.com>
2021-05-16 23:42:32 -0300

    Clean indentation

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { Plugin } from 'obsidian';
 
 export default class EmacsKeymap extends Plugin {
-	async onload() {
+    async onload() {
         CodeMirror.keyMap.emacsy['Alt-F'] = 'goWordRight';
         CodeMirror.keyMap.emacsy['Alt-B'] = 'goWordLeft';
         CodeMirror.keyMap.emacsy['Alt-D'] = 'delWordAfter';
     }
 
-	async onunload() {
+    async onunload() {
         delete CodeMirror.keyMap.emacsy['Alt-F']
         delete CodeMirror.keyMap.emacsy['Alt-B']
         delete CodeMirror.keyMap.emacsy['Alt-D']
-	}
+    }
 }
